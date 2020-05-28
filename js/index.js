53126d6ec4c12398639a14be9ad3828ece69e682
Tarun Juyal <tarunJuyalHere@gmail.com>
2020-05-28 13:45:46 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,58 @@
+function getHistory() {
+  return document.querySelector("#resultHistory").innerText;
+}
+function printHistory(valueHistory) {
+  document.querySelector("#resultHistory").innerText = valueHistory;
+}
+
+function getResult() {
+  return document.querySelector("#result").innerText;
+}
+function printResult(valueResult) {
+  document.querySelector("#result").innerText = valueResult;
+}
+addEventListener("load", start);
+
+function start() {
+  var operators = document.getElementsByClassName("operator");
+  for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener("click", function () {
+      if (this.id == "backspace") {
+        let output = getResult();
+        if (output) {
+          output = output.substr(0, output.length - 1);
+          printResult(output);
+        }
+      } else if (this.id == "clear") {
+        printResult("");
+        printHistory("");
+      } else {
+        let output = getResult();
+        let history = getHistory();
+        if (output) {
+          history += output;
+          if (this.id == "=") {
+            let result = eval(history);
+            printResult(result);
+            printHistory("");
+          } else {
+            history += this.id;
+            printHistory(history);
+            printResult("");
+          }
+        }
+      }
+    });
+  }
+
+  var numbers = document.getElementsByClassName("number");
+  for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener("click", function () {
+      var output = getResult();
+      if (output != NaN) {
+        output += this.id;
+        printResult(output);
+      }
+    });
+  }
+}
